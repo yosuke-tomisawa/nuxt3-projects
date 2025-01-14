17a76e86b08138ccef1c78199872920ec5074149
yosuke.tomisawa <bhav.urs@gmail.com>
2025-01-14 15:22:04 +0900

    img,json

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <h1>users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.id }}, {{ user.name }}
+      </li>
+    </ul>
+    <img
+      src="~/assets/janko-ferlic-ZzVMNgg23pk-unsplash.jpg"
+      alt="cycle on girl"
+    />
   </div>
 </template>
+<script setup>
+const { data: users } = await useFetch(
+  "https://jsonplaceholder.typicode.com/users"
+);
+</script>
